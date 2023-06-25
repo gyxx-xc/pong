7f8b4400ca62ed6723e0828cb82490a157ad319c
gyxx-xc <xdll_xc@outlook.com>
2023-06-25 12:51:36 -0700

    setup mod

---
+++ src/main/java/org/pongdev/pong/Pong.java
@@ -0,0 +1,11 @@
+package org.pongdev.pong;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Pong.MODID)
+public class Pong {
+    public static final String MODID = "pong";
+    public Pong(){
+
+    }
+}
