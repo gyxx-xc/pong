d8979452c119a526ffa7f9e6c5ba7879f08f49f4
gyxx-xc <xdll_xc@outlook.com>
2023-06-26 09:19:34 -0700

    build: change mapping to parchment

--- src/main/java/org/pongdev/pong/item/Champagne.java
+++ src/main/java/org/pongdev/pong/item/Champagne.java
@@ -12,7 +12,6 @@ import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
-import org.pongdev.pong.Pong;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
