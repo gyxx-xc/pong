1a18d5fe1ec2435b972dfd858eaf5eb88af0a23c
gyxx-xc <xdll_xc@outlook.com>
2023-07-01 23:07:10 -0700

    feat: add two blocks

---
+++ src/main/java/org/pongdev/pong/block/ChampagneTower.java
@@ -0,0 +1,20 @@
+package org.pongdev.pong.block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class ChampagneTower extends Block implements EntityBlock {
+    public ChampagneTower(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
+        return null;
+    }
+}
