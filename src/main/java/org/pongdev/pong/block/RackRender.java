c98338bd964defa196b41ebaef1e07a799a15846
gyxx-xc <xdll_xc@outlook.com>
2023-07-03 18:12:07 -0700

    feat: set up the render of champagne rack

---
+++ src/main/java/org/pongdev/pong/block/RackRender.java
@@ -0,0 +1,30 @@
+package org.pongdev.pong.block;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.CubeDeformation;
+import net.minecraft.client.model.geom.builders.CubeListBuilder;
+import net.minecraft.client.model.geom.builders.MeshDefinition;
+import net.minecraft.client.model.geom.builders.PartDefinition;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+
+public class RackRender implements BlockEntityRenderer<RackEntity> {
+    public RackRender(BlockEntityRendererProvider.Context context) {
+    }
+
+    @Override
+    public void render(RackEntity pBlockEntity, float pPartialTick, PoseStack pPoseStack, MultiBufferSource pBufferSource, int pPackedLight, int pPackedOverlay) {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+        PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main",
+                CubeListBuilder.create()
+                        .texOffs(0, 0)
+                        .addBox(-3.0F, -2.0F, -1.0F, 2.0F, 2.0F, 2.0F,
+                                new CubeDeformation(0.0F)),
+                PartPose.offset(0.0F, 24.0F, 0.0F));
+        bb_main.bake(16, 16).render(pPoseStack, pBufferSource.getBuffer(RenderType.solid()), pPackedLight, pPackedOverlay);
+    }
+}
