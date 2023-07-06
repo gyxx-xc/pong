ae23b2f534a3474398a5555d84847a2f066f83a6
yy <yuanye@Yuan-YeMacBook-Pro.local>
2023-07-06 14:30:23 +0800

    Add basic particle effects

--- src/main/java/org/pongdev/pong/item/ChampagneBottle.java
+++ src/main/java/org/pongdev/pong/item/ChampagneBottle.java
@@ -1,7 +1,9 @@
 package org.pongdev.pong.item;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
@@ -11,9 +13,11 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import org.jetbrains.annotations.NotNull;
+import org.pongdev.pong.particle.ModParticles;
 import org.pongdev.pong.setup.Registration;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -85,4 +89,24 @@ public class ChampagneBottle extends Item {
         pStack.shrink(1);
         return pStack;
     }
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        if (pContext.getLevel().isClientSide()) {
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+
+            spawnFoundParticles(pContext, positionClicked);
+        }
+        return super.useOn(pContext);
+    }
+
+        private void spawnFoundParticles(UseOnContext pContext, BlockPos positionClicked) {
+            for(int i = 0; i < 360; i++) {
+                if(i % 20 == 0) {
+                    pContext.getLevel().addParticle(ModParticles.SPLASH_PARTICLES.get(),
+                            positionClicked.getX() + 0.5d, positionClicked.getY() + 1, positionClicked.getZ() + 0.5d,
+                            Math.cos(i) * 0.15d, 0.15d, Math.sin(i) * 0.15d);
+                }
+            }
+        }
 }
---
+++ src/main/java/org/pongdev/pong/particle/custom/SplashParticles.java
@@ -0,0 +1,58 @@
+package org.pongdev.pong.particle.custom;
+
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.particle.*;
+import net.minecraft.core.particles.SimpleParticleType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class SplashParticles extends TextureSheetParticle {
+    protected SplashParticles(ClientLevel pLevel, double pX, double pY, double pZ,
+                              SpriteSet spriteSet, double pXSpeed, double pYSpeed, double pZSpeed) {
+        super(pLevel, pX, pY, pZ, pXSpeed, pYSpeed, pZSpeed);
+
+        this.friction = 0.8F;
+        this.xd = pXSpeed;
+        this.yd = pYSpeed;
+        this.zd = pZSpeed;
+        this.quadSize *= 0.85F;
+        this.lifetime = 20;
+        this.setSpriteFromAge(spriteSet);
+
+        this.rCol = 1f;
+        this.gCol = 1f;
+        this.bCol = 1f;
+
+
+    }
+
+    @Override
+    public void tick() {
+        super.tick();
+        fadeout();
+    }
+
+    private void fadeout(){
+        this.alpha = (-(1/(float)lifetime) * age + 1);
+    }
+
+    @Override
+    public ParticleRenderType getRenderType() {
+        return ParticleRenderType.PARTICLE_SHEET_TRANSLUCENT;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static class Provider implements ParticleProvider<SimpleParticleType> {
+        private final SpriteSet sprites;
+
+        public Provider(SpriteSet spriteSet) {
+            this.sprites = spriteSet;
+        }
+
+        public Particle createParticle(SimpleParticleType particleType, ClientLevel level,
+                                       double x, double y, double z,
+                                       double dx, double dy, double dz) {
+            return new SplashParticles(level, x, y, z, this.sprites, dx, dy, dz);
+        }
+    }
+}
