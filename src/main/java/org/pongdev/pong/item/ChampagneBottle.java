c2eee1cb817647c13fb59599b6f24c3891ac595e
gyxx-xc <xdll_xc@outlook.com>
2023-07-10 19:19:13 -0700

    merge from master

--- src/main/java/org/pongdev/pong/item/ChampagneBottle.java
+++ src/main/java/org/pongdev/pong/item/ChampagneBottle.java
@@ -2,27 +2,17 @@ package org.pongdev.pong.item;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
-import org.jetbrains.annotations.NotNull;
-import org.pongdev.pong.Pong;
-import org.pongdev.pong.block.ChampagneRack;
 import org.pongdev.pong.block.RackEntity;
+import org.pongdev.pong.particle.ModParticles;
 import org.pongdev.pong.setup.Registration;
 
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -87,6 +77,17 @@ public class ChampagneBottle extends BlockItem {
         else
             return 64;
     }
+
+    private void spawnFoundParticles(UseOnContext pContext, BlockPos positionClicked) {
+        for(int i = 0; i < 360; i++) {
+            if(i % 20 == 0) {
+                pContext.getLevel().addParticle(ModParticles.SPLASH_PARTICLES.get(),
+                        positionClicked.getX() + 0.5d, positionClicked.getY() + 1, positionClicked.getZ() + 0.5d,
+                        Math.cos(i) * 0.15d, 0.15d, Math.sin(i) * 0.15d);
+            }
+        }
+    }
+
     @Override
     public InteractionResult useOn(UseOnContext pContext) {
         if (pContext.getLevel().isClientSide()) {
@@ -95,21 +96,6 @@ public class ChampagneBottle extends BlockItem {
 
             spawnFoundParticles(pContext, positionClicked);
         }
-        return super.useOn(pContext);
-    }
-
-        private void spawnFoundParticles(UseOnContext pContext, BlockPos positionClicked) {
-            for(int i = 0; i < 360; i++) {
-                if(i % 20 == 0) {
-                    pContext.getLevel().addParticle(ModParticles.SPLASH_PARTICLES.get(),
-                            positionClicked.getX() + 0.5d, positionClicked.getY() + 1, positionClicked.getZ() + 0.5d,
-                            Math.cos(i) * 0.15d, 0.15d, Math.sin(i) * 0.15d);
-                }
-            }
-        }
-
-    @Override
-    public InteractionResult useOn(UseOnContext pContext) {
         if (!pContext.getItemInHand().getOrCreateTag().getBoolean(OPEN_TAG)) {
             Level level = pContext.getLevel();
             BlockPos pos = pContext.getClickedPos();
