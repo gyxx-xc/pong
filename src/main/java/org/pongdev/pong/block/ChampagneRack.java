1cbec72d9d1d8b0e902ab2956f6dd673fea6fd62
gyxx-xc <xdll_xc@outlook.com>
2023-07-25 15:47:02 -0700

    can pick up champagne now

--- src/main/java/org/pongdev/pong/block/ChampagneRack.java
+++ src/main/java/org/pongdev/pong/block/ChampagneRack.java
@@ -1,7 +1,13 @@
 package org.pongdev.pong.block;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -10,7 +16,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
+import org.pongdev.pong.setup.Registration;
 
 public class ChampagneRack extends HorizontalDirectionalBlock implements EntityBlock {
     public static final String ID = "champagne_rack";
@@ -45,5 +53,20 @@ public class ChampagneRack extends HorizontalDirectionalBlock implements EntityB
     @Override
     public BlockEntity newBlockEntity(BlockPos pPos, BlockState pState) {
         return new RackEntity(pPos, pState);
+    }
+
+    @Override
+    public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
+        CompoundTag tag = pLevel.getBlockEntity(pPos).getPersistentData();
+        ItemStack itemStack = pPlayer.getItemInHand(pHand);
+        if (!itemStack.isEmpty()) return InteractionResult.FAIL;
+        if (tag.getInt(CONTAIN) <= 0) return InteractionResult.FAIL;
+        if (itemStack.isEmpty()) {
+            ItemStack newItemStack = new ItemStack(Registration.CHAMPAGNE.get());
+            pPlayer.getInventory().add(newItemStack);
+            tag.putInt(CONTAIN, tag.getInt(CONTAIN)-1);
+            return InteractionResult.SUCCESS;
+        }
+        return InteractionResult.PASS;
     }
 }
