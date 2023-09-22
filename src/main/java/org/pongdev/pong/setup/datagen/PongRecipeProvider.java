1079731c85c97eb20dee51d2624d9de0f9e7b5aa
Rokidna G <qq617462323@outlook.com>
2023-09-22 19:24:19 +0800

    a

--- src/main/java/org/pongdev/pong/setup/datagen/PongRecipeProvider.java
+++ src/main/java/org/pongdev/pong/setup/datagen/PongRecipeProvider.java
@@ -1,2 +1,32 @@
-package org.pongdev.pong.setup.datagen;public class PongRecipeProvider {
+package org.pongdev.pong.setup.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.tags.ItemTags;
+import org.pongdev.pong.Pong;
+import org.pongdev.pong.setup.Registration;
+
+import java.util.function.Consumer;
+
+public class PongRecipeProvider extends RecipeProvider {
+    public PongRecipeProvider(PackOutput pOutput) {
+
+        super(pOutput);
+    }
+
+    @Override
+    protected void buildRecipes(Consumer<FinishedRecipe> pWriter) {
+        ShapedRecipeBuilder
+                .shaped(RecipeCategory.DECORATIONS, Registration.PLUG.get())
+                .define('#', ItemTags.PLANKS)
+                .pattern(" # ")
+                .pattern("###")
+                .group("pong")
+                .unlockedBy("has_logs",has(ItemTags.PLANKS))
+                .save(pWriter);
+
+    }
 }
